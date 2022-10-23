a13c20a5fcef027146fdf46da4c1850f3b3fd600
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-10-23 18:51:59 +0900

    FND_Cont.c
    7-segment(fnd) controll
    
    controller device : TM74HC595

---
+++ FND_Cont.c
@@ -0,0 +1,526 @@
+//main.c
+
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "fnd_controller.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  init_fnd();
+
+  /*
+  volatile unsigned int * reg = 0x40021018;
+  *reg |= 16;
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_LED_Pin; //PC13 1 << 13
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIO_LED_GPIO_Port, &GPIO_InitStruct);
+
+  volatile unsigned int * reg2 = 0x40011004;
+  *reg2 = (*reg2 & ~(15UL << 20U)) | (3U << 20U);
+  */
+
+
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  /*volatile unsigned int * reg3 = 0x40011010;*/
+
+  //char senddata[20] = "hello world\r\n";
+
+
+  while (1)
+  {
+
+	  for(int i = 0; i <= 99; i++){
+		  digit2(i, 0b001, 50);
+	  }
+
+	  /*
+	  if(!HAL_GPIO_ReadPin(PB0_TEMP_SET_UP_GPIO_Port, PB0_TEMP_SET_UP_Pin)){
+		  HAL_GPIO_WritePin(PB6_LED1_GPIO_Port, PB6_LED1_Pin, 0);
+	  }
+	  else{
+		  HAL_GPIO_WritePin(PB6_LED1_GPIO_Port, PB6_LED1_Pin, 1);
+	  }
+	  HAL_Delay(500);
+	  */
+
+
+
+	  //HAL_GPIO_WritePin(PB6_LED1_GPIO_Port, PB6_LED1_Pin, 0);
+	  //HAL_Delay(1000);
+	  //HAL_GPIO_WritePin(PB6_LED1_GPIO_Port, PB6_LED1_Pin, 1);
+	  //HAL_Delay(1000);
+
+
+	  //HAL_UART_Transmit(&huart1, senddata, strlen(senddata), 1000);
+
+	  //HAL_Delay(1000);
+
+	  /*
+	  *reg3 = 0x2000;
+
+	  HAL_Delay(100);
+
+	  *reg3 = (0x2000 << 16);
+	  HAL_Delay(100);
+	  */
+
+	  /*HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, 1);
+	  HAL_Delay(500);
+	  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, 0);
+	  HAL_Delay(50);*/
+
+	  /*
+
+	  if(!HAL_GPIO_ReadPin(GPIO_SW_GPIO_Port, GPIO_SW_Pin)){
+		  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, 0);
+	  }else{
+		  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, 1);
+	  }
+	  HAL_Delay(100);
+	  */
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, FND_RCLK_Pin|FND_DIO_Pin|FND_SCLK_Pin|PB6_LED1_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : GPIO_LED_Pin */
+  GPIO_InitStruct.Pin = GPIO_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIO_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : GPIO_SW_Pin */
+  GPIO_InitStruct.Pin = GPIO_SW_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIO_SW_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB0_TEMP_SET_UP_Pin */
+  GPIO_InitStruct.Pin = PB0_TEMP_SET_UP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(PB0_TEMP_SET_UP_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : FND_RCLK_Pin FND_DIO_Pin FND_SCLK_Pin PB6_LED1_Pin */
+  GPIO_InitStruct.Pin = FND_RCLK_Pin|FND_DIO_Pin|FND_SCLK_Pin|PB6_LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+//--------------------------------------------------------------------------------------
+//------------------------------main.c end ---------------------------------------------
+//--------------------------------------------------------------------------------------
+
+
+//fnd_controller.c
+
+#include "fnd_controller.h"
+
+
+uint8_t _LED_0F[29];
+
+void init_fnd()
+{
+_LED_0F[0] = 0xC0; //0
+_LED_0F[1] = 0xF9; //1
+_LED_0F[2] = 0xA4; //2
+_LED_0F[3] = 0xB0; //3
+_LED_0F[4] = 0x99; //4
+_LED_0F[5] = 0x92; //5
+_LED_0F[6] = 0x82; //6
+_LED_0F[7] = 0xF8; //7
+_LED_0F[8] = 0x80; //8
+_LED_0F[9] = 0x90; //9
+_LED_0F[10] = 0x88; //A
+_LED_0F[11] = 0x83; //b
+_LED_0F[12] = 0xC6;
+_LED_0F[13] = 0xA1;
+_LED_0F[14] = 0x86;
+_LED_0F[15] = 0x8E;
+_LED_0F[16] = 0xC2;
+_LED_0F[17] = 0x89;
+_LED_0F[18] = 0xF9;
+_LED_0F[19] = 0xF1; //J
+_LED_0F[20] = 0xC3; //L
+_LED_0F[21] = 0xA9; //n
+_LED_0F[22] = 0xC0; //O
+_LED_0F[23] = 0x8C; //P
+_LED_0F[24] = 0x98; //q
+_LED_0F[25] = 0x92; //S
+_LED_0F[26] = 0xC1; //U
+_LED_0F[27] = 0x91; //Y
+_LED_0F[28] = 0xFE; //hight -
+}
+
+
+void send(uint8_t x)
+{
+
+	for(int i = 8; i >= 1; i--)
+	{
+		if(x & 0x80)
+		{
+			HAL_GPIO_WritePin(FND_DIO_GPIO_Port, FND_DIO_Pin, HIGH);
+		}
+		else
+		{
+			HAL_GPIO_WritePin(FND_DIO_GPIO_Port, FND_DIO_Pin, LOW);
+		}
+		x <<= 1;
+		HAL_GPIO_WritePin(FND_SCLK_GPIO_Port, FND_SCLK_Pin, LOW);
+		HAL_GPIO_WritePin(FND_SCLK_GPIO_Port, FND_SCLK_Pin, HIGH);
+
+	}
+
+}
+
+
+void send_port(uint8_t x, uint8_t port)
+{
+
+	send(x);
+	send(port);
+	HAL_GPIO_WritePin(FND_RCLK_GPIO_Port, FND_RCLK_Pin, LOW);
+	HAL_GPIO_WritePin(FND_RCLK_GPIO_Port, FND_RCLK_Pin, HIGH);
+
+}
+
+void digit4_show(int n, int replay, uint8_t showZero)
+{
+	int n1, n2, n3, n4;
+	n1 = (int) n % 10;
+	n2 = (int) ((n % 100) - n1) / 10;
+	n3 = (int) ((n % 1000) - n2 - n1) / 100;
+	n4 = (int) ((n % 10000) - n3 - n2 - n1) / 1000;
+
+	for(int i = 0; i <= replay; i++){
+		send_port(_LED_0F[n1], 0b0001);
+		if(showZero | n>9) send_port(_LED_0F[n2], 0b0010);
+		if(showZero | n>99) send_port(_LED_0F[n3], 0b0100);
+		if(showZero | n>999) send_port(_LED_0F[n4], 0b1000);
+	}
+}
+
+void digit4_replay(int n, int replay)
+{
+	digit4_show(n, replay, false);
+}
+
+void digit4(int n)
+{
+	digit4_show(n, 0, false);
+}
+
+void digit4showZero_replay(int n, int replay)
+{
+	digit4_show(n, replay, true);
+}
+
+void digit4showZero(int n)
+{
+	digit4_show(n, 0, true);
+}
+
+void digit2(int n, int port, int replay)
+{
+	int n1, n2;
+	n1 = (int) n % 10;
+	n2 = (int) ((n % 100) - n1)/10;
+
+	for(int i = 0; i <= replay; i++){
+		send_port(_LED_0F[n1], port);
+		send_port(_LED_0F[n2], port<<1);
+	}
+}
+
+void digit2_port(int n, int port)
+{
+	digit2(n, port, 0);
+}
+
+//--------------------------------------------------------------------------------------
+//------------------------------fnd_controller.c end -----------------------------------
+//--------------------------------------------------------------------------------------
+
+
+//fnd_controller.h
+
+/*
+ * fnd_controller.h
+ *
+ *  Created on: Oct 23, 2022
+ *      Author: JHKIm
+ */
+
+#ifndef SRC_FND_CONTROLLER_H_
+#define SRC_FND_CONTROLLER_H_
+
+#include "main.h"
+
+
+#define HIGH 1
+#define LOW 0
+
+#define false 0
+#define true 1
+
+
+
+void send(uint8_t x);
+
+void init_fnd();
+
+void send_port(uint8_t x, uint8_t port);
+
+void digit4_show(int n, int replay, uint8_t showZero);
+
+void digit4_replay(int n, int replay);
+
+void digit4(int n);
+
+void digit4showZero_replay(int n, int replay);
+
+void digit4showZero(int n);
+
+void digit2(int n, int port, int replay);
+
+void digit2_port(int n, int port);
+
+
+#endif /* SRC_FND_CONTROLLER_H_ */
+
+
+
+//--------------------------------------------------------------------------------------
+//------------------------------fnd_controller.h end -----------------------------------
+//--------------------------------------------------------------------------------------
+
+
+
